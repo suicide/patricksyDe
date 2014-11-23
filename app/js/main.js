f2f4484c84ee63f7bdb8acc8d5f186097982bee4
suicide <suicide@get-it.us>
2014-11-23 23:52:09 +0100

    moved js code to own file

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,21 @@
-/* dummy */
+/**
+ * handle resizes and change the picture
+ */
+$(document).ready(function() {
+
+  var theWindow = $(window);
+  var image = $('#portrait');
+
+  function checkWidth() {
+    var windowSize = theWindow.width();
+    // why??
+    if (windowSize > 752) {
+      image.attr('src', 'img/psy_the_face_200.jpg');
+    } else {
+      image.attr('src', 'img/psy_the_face_small.jpg');
+    }
+  }
+
+  checkWidth();
+  theWindow.resize(checkWidth);
+});
